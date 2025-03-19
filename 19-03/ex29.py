7dccfcb5bd4140c5bee0cfb90c87dc285ab47792
Murilo29120 <murilo.mendes.rodrigues@escola.pr.gov.br>
2025-03-19 11:02:11 -0300

    Add files via upload

--- 19-03/ex29.py
+++ 19-03/ex29.py
@@ -1,38 +1,42 @@
 def exibir_menu():
-        print("Bem vindo ao menu de cadastro")
+        print("Bem-vindo ao menu de cadastro")
         print("1- Novo Cadastro")
         print("2- Ver Cadastro")
         print("3- Sair")
         print("-------------------------------------------")
 
-def cadastrar_pessoa (cadastrado):
-        nome= input("nome:")
-        idade= input("idade:")
-        curso= input("curso:")
-        cadastros.append({"nome": nome, "Idade": idade,"Turma":turma, "Curso": curso})
-        print("Cadastro realizado com sucesso!")
+def cadastrar_pessoa(cadastros):
+    nome = input("Nome: ")
+    idade = input("Idade: ")
+    curso = input("Curso: ")
+    turma = input("Turma: ")  
+    cadastros.append({"Nome": nome, "Idade": idade, "Turma": turma, "Curso": curso})
+    print("Cadastro realizado com sucesso!")
 
-
-        def ver_cadastros(cadastros):
-            if not cadastros:
-                print("Nenhum cadastro no sistema.")
-            else:
-                print("\n------ LISTA DE CADASTROS------")
-                for i, pessoa in enumerate (cadastros, 1):
-                    print(F"{i}. Nome:{pessoa['Nome']}, Idade:
-{pessoa ['Idade']}, Turma: {pessoa['Turma']}, Curso: {pessoa['Curso']})
-
+def ver_cadastros(cadastros):
+    if not cadastros:
+        print("Nenhum cadastro no sistema.")
+    else:
+        print("\n------ LISTA DE CADASTROS ------")
+        for i, pessoa in enumerate(cadastros, 1):
+            print(f"{i}. Nome: {pessoa['Nome']}, Idade: {pessoa['Idade']}, Turma: {pessoa['Turma']}, Curso: {pessoa['Curso']}")
 
 def main():
-    cadastros= []
+    cadastros = []
     while True:
-        exibir_menu ()
-        opção= input("Escolha uma opção: ")
-        if opção == "1":
+        exibir_menu()
+        opcao = input("Escolha uma opção: ")
+        if opcao == "1":
             cadastrar_pessoa(cadastros)
-        elif opção == "2":
+        elif opcao == "2":
             ver_cadastros(cadastros)
-        elif opção == "3":
-            print("Obrigado por utilizar nosso sistemas!")      
+        elif opcao == "3":
+            print("Obrigado por utilizar nosso sistema!")      
             break      
+        else:
+            print("Opção incorreta! Tente novamente.")
+
+if __name__ == "__main__":
+    main()
+
         
